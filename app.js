7ffc515e2f2da1ffbfec2e228f7c7e7685fe29f3
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-01 17:27:40 +0100

    fixed regex validation

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const input = document.querySelector('#email-input--address');
+const input = document.querySelector("#email-input--address");
 const btn = document.querySelector(".email-input--button");
-const errorImg = document.querySelector('.error-img');
+const errorImg = document.querySelector(".error-img");
 const errorText = document.querySelector(".error-text");
+const successText = document.querySelector(".success-text");
 
-console.log(input)
-console.log(btn)
-console.log(errorImg)
-console.log(errorText)
-
-btn.addEventListener('click', validateEmail);
-
-function validateEmail(inputText) {
-    const mailRgx = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/
-
-    if (inputText.value !== mailRgx) {
-        errorImg.classList.add('warning');
-        errorText.classList.add('warning');
-        return false;
-    }
-}
+btn.addEventListener("click", () => {
+    mailRgx = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+  if (mailRgx.test(input.value)) {
+      errorImg.classList.remove("warning");
+      errorText.classList.remove("warning");
+    successText.classList.add("success");
+    return true;
+  } else {
+    errorImg.classList.add("warning");
+    errorText.classList.add("warning");
+    return false;
+  }
+});
